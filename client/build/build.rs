30e4e7d9dcb901891f9ab163ae2be8e1e23b8ddc
Edwin Svensson <git@olback.net>
2020-02-06 22:20:11 +0100

    add build step

---
+++ client/build/build.rs
@@ -0,0 +1,16 @@
+mod out;
+mod resources;
+mod glade;
+mod version;
+
+fn main() {
+
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed=assets/*");
+
+    out::output_dir();
+    version::get_version();
+    glade::fix_resource_paths();
+    // resources::generate_resources();
+
+}
