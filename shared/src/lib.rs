6c2c18996525269212b86608538eb2f84d3c3790
Edwin Svensson <git@olback.net>
2020-02-21 01:52:20 +0100

    create types for user id/hash

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,5 +1,8 @@
 use serde::{Serialize, Deserialize};
 
+pub type UserHash = [u8; 32];
+pub type UserId = [u8; 32];
+
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 pub enum AfkStatus {
     Available,
@@ -9,18 +12,18 @@ pub enum AfkStatus {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Message {
-    pub to: [u8; 32], // Friend Hash
+    pub to: UserHash, // Friend Hash
     pub data: Vec<u8> // RSA(enum MessageData { Message(String), Poke })
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ToServer {
-    KeepAlive([u8; 32], Vec<[u8; 32]>, AfkStatus), // My id, Vec<Friend hash>
+    KeepAlive(UserId, Vec<UserHash>, AfkStatus), // My id, Vec<Friend hash>
     Message(Message)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ToClient {
-    FriendsOnline(Vec<([u8; 32], AfkStatus)>), //  Vec<Freind hash>
+    FriendsOnline(Vec<(UserHash, AfkStatus)>), //  Vec<Freind hash>
     Message(Vec<u8>)
 }
