01ca18432961486666ceb4b23389a43c091b1df3
Edwin Svensson <git@olback.net>
2020-06-21 03:42:53 +0200

    only allow TLS 1.2 and greater

--- client/src/utils/tls_stream.rs
+++ client/src/utils/tls_stream.rs
@@ -2,7 +2,7 @@ use {
     crate::{error::BariumResult, consts::TCP_TIMEOUT},
     super::get_srv_addr,
     std::{net::{TcpStream, ToSocketAddrs}, time::Duration},
-    native_tls::{TlsConnector, TlsStream}
+    native_tls::{TlsConnector, TlsStream, Protocol}
 };
 
 pub fn new_tls_stream(address: String, port: u16, allow_invalid_cert: bool) -> BariumResult<TlsStream<TcpStream>> {
@@ -12,7 +12,10 @@ pub fn new_tls_stream(address: String, port: u16, allow_invalid_cert: bool) -> B
     let addr = format!("{}:{}", host, port).to_socket_addrs()?.nth(0).unwrap();
 
     let stream = TcpStream::connect_timeout(&addr, Duration::from_secs(TCP_TIMEOUT))?;
-    let tls_connector = TlsConnector::builder().danger_accept_invalid_certs(allow_invalid_cert).build()?;
+    let tls_connector = TlsConnector::builder()
+        .min_protocol_version(Some(Protocol::Tlsv12)) // TODO Tlsv13
+        .danger_accept_invalid_certs(allow_invalid_cert)
+        .build()?;
     let tls_stream = tls_connector.connect(host.as_str(), stream)?;
 
     Ok(tls_stream)
